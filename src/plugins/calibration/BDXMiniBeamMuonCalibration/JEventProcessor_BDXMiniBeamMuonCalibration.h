b559dc86f318977934d5cd78d667d962e9c1d90b
Andrea Celentano <andrea.celentano@ge.infn.it>
2020-02-18 16:22:33 +0100

    BDXMiniMuonCalibration m_isMC and root output are there

--- src/plugins/calibration/BDXMiniBeamMuonCalibration/JEventProcessor_BDXMiniBeamMuonCalibration.h
+++ src/plugins/calibration/BDXMiniBeamMuonCalibration/JEventProcessor_BDXMiniBeamMuonCalibration.h
@@ -9,19 +9,27 @@
 #define _JEventProcessor_BDXMiniBeamMuonCalibration_
 
 #include <JANA/JEventProcessor.h>
+class JROOTOutput;
 
-class JEventProcessor_BDXMiniBeamMuonCalibration:public jana::JEventProcessor{
-	public:
-		JEventProcessor_BDXMiniBeamMuonCalibration();
-		~JEventProcessor_BDXMiniBeamMuonCalibration();
-		const char* className(void){return "JEventProcessor_BDXMiniBeamMuonCalibration";}
+class JEventProcessor_BDXMiniBeamMuonCalibration: public jana::JEventProcessor {
+public:
+	JEventProcessor_BDXMiniBeamMuonCalibration();
+	~JEventProcessor_BDXMiniBeamMuonCalibration();
+	const char* className(void) {
+		return "JEventProcessor_BDXMiniBeamMuonCalibration";
+	}
 
-	private:
-		jerror_t init(void);						///< Called once at program start.
-		jerror_t brun(jana::JEventLoop *eventLoop, int32_t runnumber);	///< Called everytime a new run number is detected.
-		jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
-		jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
-		jerror_t fini(void);						///< Called after last event of last event source has been processed.
+private:
+	jerror_t init(void);						///< Called once at program start.
+	jerror_t brun(jana::JEventLoop *eventLoop, int32_t runnumber);	///< Called everytime a new run number is detected.
+	jerror_t evnt(jana::JEventLoop *eventLoop, uint64_t eventnumber);	///< Called every event.
+	jerror_t erun(void);						///< Called everytime run number changes, provided brun has been called.
+	jerror_t fini(void);						///< Called after last event of last event source has been processed.
+
+	int m_isFirstCallToBrun;
+
+	JROOTOutput *m_ROOTOutput;
+	int m_isMC;
 };
 
 #endif // _JEventProcessor_BDXMiniBeamMuonCalibration_
