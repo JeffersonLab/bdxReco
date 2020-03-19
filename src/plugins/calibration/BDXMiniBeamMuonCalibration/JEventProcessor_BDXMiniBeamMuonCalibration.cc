c02cfd18240f67f422433c7d332ab255953fefba
Luca Marsicano <luca.marsicano@ge.infn.it>
2020-03-19 10:43:46 +0100

    Plugin for calibration with muon beam

--- src/plugins/calibration/BDXMiniBeamMuonCalibration/JEventProcessor_BDXMiniBeamMuonCalibration.cc
+++ src/plugins/calibration/BDXMiniBeamMuonCalibration/JEventProcessor_BDXMiniBeamMuonCalibration.cc
@@ -18,6 +18,10 @@ using namespace jana;
 //Add the factories for bdxReco
 #include "DAQ/eventData.h"
 #include "Calorimeter/CalorimeterHit.h"
+#include "IntVeto/IntVetoDigiHit.h"
+
+#include "TH1D.h"
+#include "TH2D.h"
 
 extern "C" {
 void InitPlugin(JApplication *app) {
@@ -48,13 +52,110 @@ jerror_t JEventProcessor_BDXMiniBeamMuonCalibration::init(void) {
 	// and filling historgrams in this plugin, you should lock the
 	// ROOT mutex like this:
 	//
-	// japp->RootWriteLock();
+	//japp->RootWriteLock();
 	//  ... fill historgrams or trees ...
 	// japp->RootUnLock();
 
 	m_isFirstCallToBrun = 1;
 	m_isMC = 0;
 	gPARMS->GetParameter("MC", m_isMC);
+
+//	japp->RootWriteLock();
+	hTrj_all_top = new TH1D("hTrj_all_top","hTrj_all_top",8,0.5,8.5);
+	hTrj_all_bot = new TH1D("hTrj_all_bot","hTrj_all_bot",8,0.5,8.5);
+
+	hTrj_corr_top = new TH2D("hTrj_corr_top","hTrj_corr_top",8,0.5,8.5,8,0.5,8.5);
+	hTrj_corr_bot = new TH2D("hTrj_corr_bot","hTrj_corr_bot",8,0.5,8.5,8,0.5,8.5);
+
+	hTrj_corr_crs_top = new TH2D("hTrj_corr_crs_top","hTrj_corr_crs_top",8,0.5,8.5,22,0.5,22.5);
+	hTrj_corr_crs_bot = new TH2D("hTrj_corr_crs_bot","hTrj_corr_crs_bot",8,0.5,8.5,22,0.5,22.5);
+
+	hCrs_m2_p0_T1_top = new TH1D("hCrs_m2_p0_T1_top","hCrs_m2_p0_T1_top",200,0,200);
+	hCrs_m2_p1_T1_top = new TH1D("hCrs_m2_p1_T1_top","hCrs_m2_p1_T1_top",200,0,200);
+
+	hCrs_m2_m2_T2_top= new TH1D("hCrs_m2_m2_T2_top","hCrs_m2_m2_T2_top",200,0,200);
+	hCrs_m1_p1_T2_top= new TH1D("hCrs_m1_p1_T2_top","hCrs_m1_p1_T2_top",200,0,200);
+	hCrs_m1_p0_T2_top= new TH1D("hCrs_m1_p0_T2_top","hCrs_m1_p0_T2_top",200,0,200);
+	hCrs_m2_p2_T2_top= new TH1D("hCrs_m2_p2_T2_top","hCrs_m2_p2_T2_top",200,0,200);
+
+
+	hCrs_m1_m2_T3_top = new TH1D("hCrs_m1_m2_T3_top","hCrs_m1_m2_T3_top",200,0,200);
+	hCrs_m1_p0_T3_top = new TH1D("hCrs_m1_p0_T3_top","hCrs_m1_p0_T3_top",200,0,200);;
+	hCrs_m1_p1_T3_top= new TH1D("hCrs_m1_p1_T3_top","hCrs_m1_p1_T3_top",200,0,200);;
+	hCrs_m2_p2_T3_top= new TH1D("hCrs_m2_p2_T3_top","hCrs_m2_p2_T3_top",200,0,200);
+
+	hCrs_p0_m2_T4_top= new TH1D("hCrs_p0_m2_T4_top","hCrs_p0_m2_T4_top",200,0,200);
+	hCrs_p0_m1_T4_top= new TH1D("hCrs_p0_m1_T4_top","hCrs_p0_m1_T4_top",200,0,200);
+	hCrs_p0_p0_T4_top= new TH1D("hCrs_p0_p0_T4_top","hCrs_p0_p0_T4_top",200,0,200);
+	hCrs_p0_p1_T4_top= new TH1D("hCrs_p0_p1_T4_top","hCrs_p0_p1_T4_top",200,0,200);
+	hCrs_p1_p2_T4_top= new TH1D("hCrs_p1_p2_T4_top","hCrs_p1_p2_T4_top",200,0,200);
+	hCrs_p1_p3_T4_top= new TH1D("hCrs_p1_p3_T4_top","hCrs_p1_p3_T4_top",200,0,200);
+
+	hCrs_p0_m2_T5_top= new TH1D("hCrs_p0_m2_T5_top","hCrs_p0_m2_T5_top",200,0,200);
+	hCrs_p0_m1_T5_top= new TH1D("hCrs_p0_m1_T5_top","hCrs_p0_m1_T5_top",200,0,200);
+	hCrs_p0_p0_T5_top= new TH1D("hCrs_p0_p0_T5_top","hCrs_p0_p0_T5_top",200,0,200);
+	hCrs_p0_p1_T5_top= new TH1D("hCrs_p0_p1_T5_top","hCrs_p0_p1_T5_top",200,0,200);
+	hCrs_m1_p2_T5_top= new TH1D("hCrs_m1_p2_T5_top","hCrs_m1_p2_T5_top",200,0,200);
+	hCrs_m1_p3_T5_top= new TH1D("hCrs_m1_p3_T5_top","hCrs_m1_p3_T5_top",200,0,200);
+
+	hCrs_p1_m2_T6_top= new TH1D("hCrs_p1_m2_T6_top","hCrs_p1_m2_T6_top",200,0,200);
+	hCrs_p1_p0_T6_top= new TH1D("hCrs_p1_p0_T6_top","hCrs_p1_p0_T6_top",200,0,200);
+	hCrs_p1_p1_T6_top= new TH1D("hCrs_p1_p1_T6_top","hCrs_p1_p1_T6_top",200,0,200);
+	hCrs_p2_p2_T6_top= new TH1D("hCrs_p2_p2_T6_top","hCrs_p2_p2_T6_top",200,0,200);
+
+	hCrs_p2_m2_T7_top= new TH1D("hCrs_p2_m2_T7_top","hCrs_p2_m2_T7_top",200,0,200);
+	hCrs_p1_p0_T7_top= new TH1D("hCrs_p1_p0_T7_top","hCrs_p1_p0_T7_top",200,0,200);
+	hCrs_p1_p1_T7_top= new TH1D("hCrs_p1_p1_T7_top","hCrs_p1_p1_T7_top",200,0,200);
+	hCrs_p2_p2_T7_top= new TH1D("hCrs_p2_p2_T7_top","hCrs_p2_p2_T7_top",200,0,200);
+
+	hCrs_p2_p0_T8_top= new TH1D("hCrs_p2_p0_T8_top","hCrs_p2_p0_T8_top",200,0,200);
+	hCrs_p2_p1_T8_top= new TH1D("hCrs_p2_p1_T8_top","hCrs_p2_p1_T8_top",200,0,200);
+
+
+
+
+	hCrs_m2_p0_T1_bot = new TH1D("hCrs_m2_p0_T1_bot","hCrs_m2_p0_T1_bot",200,0,200);
+	hCrs_m2_p1_T1_bot = new TH1D("hCrs_m2_p1_T1_bot","hCrs_m2_p1_T1_bot",200,0,200);
+
+	hCrs_m2_m2_T2_bot= new TH1D("hCrs_m2_m2_T2_bot","hCrs_m2_m2_T2_bot",200,0,200);
+	hCrs_m1_p0_T2_bot= new TH1D("hCrs_m1_p0_T2_bot","hCrs_m1_p0_T2_bot",200,0,200);
+	hCrs_m1_p1_T2_bot= new TH1D("hCrs_m1_p1_T2_bot","hCrs_m1_p1_T2_bot",200,0,200);
+	hCrs_m2_p2_T2_bot= new TH1D("hCrs_m2_p2_T2_bot","hCrs_m2_p2_T2_bot",200,0,200);
+
+	hCrs_m1_m2_T3_bot = new TH1D("hCrs_m1_m2_T3_bot","hCrs_m1_m2_T3_bot",200,0,200);
+	hCrs_m1_p0_T3_bot = new TH1D("hCrs_m1_p0_T3_bot","hCrs_m1_p0_T3_bot",200,0,200);
+	hCrs_m1_p1_T3_bot= new TH1D("hCrs_m1_p1_T3_bot","hCrs_m1_p1_T3_bot",200,0,200);
+	hCrs_m2_p2_T3_bot= new TH1D("hCrs_m2_p2_T3_bot","hCrs_m2_p2_T3_bot",200,0,200);
+
+	hCrs_p0_m2_T4_bot= new TH1D("hCrs_p0_m2_T4_bot","hCrs_p0_m2_T4_bot",200,0,200);
+	hCrs_p0_m1_T4_bot= new TH1D("hCrs_p0_m1_T4_bot","hCrs_p0_m1_T4_bot",200,0,200);
+	hCrs_p0_p0_T4_bot= new TH1D("hCrs_p0_p0_T4_bot","hCrs_p0_p0_T4_bot",200,0,200);
+	hCrs_p0_p1_T4_bot= new TH1D("hCrs_p0_p1_T4_bot","hCrs_p0_p1_T4_bot",200,0,200);
+	hCrs_p1_p2_T4_bot= new TH1D("hCrs_p1_p2_T4_bot","hCrs_p1_p2_T4_bot",200,0,200);
+	hCrs_p1_p3_T4_bot= new TH1D("hCrs_p1_p3_T4_bot","hCrs_p1_p3_T4_bot",200,0,200);
+
+	hCrs_p0_m2_T5_bot= new TH1D("hCrs_p0_m2_T5_bot","hCrs_p0_m2_T5_bot",200,0,200);
+	hCrs_p0_m1_T5_bot= new TH1D("hCrs_p0_m1_T5_bot","hCrs_p0_m1_T5_bot",200,0,200);
+	hCrs_p0_p0_T5_bot= new TH1D("hCrs_p0_p0_T5_bot","hCrs_p0_p0_T5_bot",200,0,200);
+	hCrs_p0_p1_T5_bot= new TH1D("hCrs_p0_p1_T5_bot","hCrs_p0_p1_T5_bot",200,0,200);
+	hCrs_m1_p2_T5_bot= new TH1D("hCrs_m1_p2_T5_bot","hCrs_m1_p2_T5_bot",200,0,200);
+	hCrs_m1_p3_T5_bot= new TH1D("hCrs_m1_p3_T5_bot","hCrs_m1_p3_T5_bot",200,0,200);
+
+	hCrs_p1_m2_T6_bot= new TH1D("hCrs_p1_m2_T6_bot","hCrs_p1_m2_T6_bot",200,0,200);
+	hCrs_p1_p0_T6_bot= new TH1D("hCrs_p1_p0_T6_bot","hCrs_p1_p0_T6_bot",200,0,200);
+	hCrs_p1_p1_T6_bot= new TH1D("hCrs_p1_p1_T6_bot","hCrs_p1_p1_T6_bot",200,0,200);
+	hCrs_p2_p2_T6_bot= new TH1D("hCrs_p2_p2_T6_bot","hCrs_p2_p2_T6_bot",200,0,200);
+
+	hCrs_p2_m2_T7_bot= new TH1D("hCrs_p2_m2_T7_bot","hCrs_p2_m2_T7_bot",200,0,200);
+	hCrs_p1_p0_T7_bot= new TH1D("hCrs_p1_p0_T7_bot","hCrs_p1_p0_T7_bot",200,0,200);
+	hCrs_p1_p1_T7_bot= new TH1D("hCrs_p1_p1_T7_bot","hCrs_p1_p1_T7_bot",200,0,200);
+	hCrs_p2_p2_T7_bot= new TH1D("hCrs_p2_p2_T7_bot","hCrs_p2_p2_T7_bot",200,0,200);
+
+	hCrs_p2_p0_T8_bot= new TH1D("hCrs_p2_p0_T8_bot","hCrs_p2_p0_T8_bot",200,0,200);
+	hCrs_p2_p1_T8_bot= new TH1D("hCrs_p2_p1_T8_bot","hCrs_p2_p1_T8_bot",200,0,200);
+
+
+//		japp->RootUnLock();
 
 	return NOERROR;
 }
@@ -118,9 +219,9 @@ jerror_t JEventProcessor_BDXMiniBeamMuonCalibration::evnt(JEventLoop *loop, uint
 	// loop->Get(mydataclasses);
 	//
 
-	// japp->RootWriteLock();
+	//japp->RootWriteLock();
 	//  ... fill historgrams or trees ...
-	// japp->RootUnLock();
+
 	const eventData* tData;
 	if (!m_isMC) {
 		try {
@@ -131,9 +232,223 @@ jerror_t JEventProcessor_BDXMiniBeamMuonCalibration::evnt(JEventLoop *loop, uint
 		}
 	}
 
+
+
 	///.....,
 
+	////  Vetos  ////
+	int trig_veto = 1;
 
+	//const IntVetoDigiHit* m_IntVetoDigiHit;
+	//vector<const IntVetoDigiHit*> intveto_hits;
+	//vector<const IntVetoDigiHit*>::const_iterator intveto_hits_it;
+
+
+
+
+
+
+	//// Crystals ////
+
+	int iX, iY, sector;
+	double crs[22][2] = {0};
+    //Trajectories (0 -> top, 1-> bottom):
+	int Tr[8][2] = {0};
+    //treshold
+	double thr = 5;
+
+
+
+
+	const CalorimeterHit* m_CaloHit;
+	vector<const CalorimeterHit*> calo_hits;
+	vector<const CalorimeterHit*>::const_iterator calo_hits_it;
+
+	loop->Get(calo_hits);
+
+	for (calo_hits_it = calo_hits.begin(); calo_hits_it != calo_hits.end(); calo_hits_it++) {
+			m_CaloHit = *calo_hits_it;
+			iX = m_CaloHit->m_channel.x;
+			iY = m_CaloHit->m_channel.y;
+			sector = m_CaloHit->m_channel.sector;
+			//cout << iX << "  " << iY << "  " << sector << "  " << m_CaloHit->E << endl;
+			int ID = getCaloIDXFromXY(iX,iY);
+			crs[ID-1][sector] = m_CaloHit->E;
+	}
+	//cout << "Event END" << endl;
+
+//trajectory check:
+
+	for(int i =0; i<2; i++){
+		if(crs[6][i]>thr && crs[11][i]>thr) Tr[0][i] = 1;  //Tr1
+		if(crs[0][i]>thr && crs[7][i]>thr && crs[12][i]>thr && crs[16][i]>thr) Tr[1][i] = 1;  //Tr2
+		if(crs[1][i]>thr && crs[7][i]>thr && crs[12][i]>thr && crs[16][i]>thr) Tr[2][i] = 1;  //Tr3
+		if(crs[2][i]>thr && crs[5][i]>thr && crs[8][i]>thr && crs[13][i]>thr && crs[18][i]>thr && crs[21][i]>thr) Tr[3][i]=1; //Tr4
+		if(crs[2][i]>thr && crs[5][i]>thr && crs[8][i]>thr && crs[13][i]>thr && crs[17][i]>thr && crs[20][i]>thr) Tr[4][i]=1; //Tr5
+		if(crs[3][i]>thr && crs[9][i]>thr && crs[14][i]>thr && crs[19][i]>thr) Tr[5][i] = 1;  //Tr6
+		if(crs[4][i]>thr && crs[9][i]>thr && crs[14][i]>thr && crs[19][i]>thr) Tr[6][i] = 1;  //Tr7
+		if(crs[10][i]>thr && crs[15][i]>thr) Tr[7][i] = 1;  //Tr8
+	}
+
+	//fill Histograms
+	double Tr_test =0;
+
+	for(int i =0; i<8; i++){
+
+		for(int j=0; j< 2; j++){
+			Tr_test += Tr[i][j];
+		//	cout << Tr[i][j] << " ";
+
+		}
+	}
+
+
+	for(int i =0; i<8; i++){
+
+			for(int j=0; j< 2; j++){
+				if(Tr[i][j]==1 && j==0){
+					hTrj_corr_top->Fill(Tr_test,i+1);
+					hTrj_all_top->Fill(i+1);
+				}
+				if(Tr[i][j]==1 && j==1) {
+					hTrj_all_bot->Fill(i+1);
+					hTrj_corr_bot->Fill(Tr_test,i+1);
+			}
+		}
+	}
+
+
+	for(int i =0; i<8; i++){
+		for(int j=0; j< 2; j++){
+			for(int k=0; k< 22; k++){
+				if(Tr[i][j]==1 && j==0 && crs[k][j]>thr){
+					hTrj_corr_crs_top->Fill(i+1,k+1);
+				}
+				if(Tr[i][j]==1 && j==1 && crs[k][j]>thr){
+					hTrj_corr_crs_bot->Fill(i+1,k+1);
+				}
+			}
+		}
+	}
+
+
+	//cout<< endl;
+
+	if(Tr_test >= 1){
+		if(trig_veto == 1){
+
+			//TOP
+			if(Tr[0][0] == 1){
+				//cout << "tr1" << endl;
+				hCrs_m2_p0_T1_top->Fill(crs[6][0]);
+				hCrs_m2_p1_T1_top->Fill(crs[11][0]);
+			}
+			if(Tr[1][0] == 1){
+				//cout << "tr2" << endl;
+				hCrs_m2_m2_T2_top->Fill(crs[0][0]);
+				hCrs_m1_p0_T2_top->Fill(crs[7][0]);
+				hCrs_m1_p1_T2_top->Fill(crs[12][0]);
+				hCrs_m2_p2_T2_top->Fill(crs[16][0]);
+			}
+			if(Tr[2][0] == 1){
+				//cout << "tr3" << endl;
+				hCrs_m1_m2_T3_top->Fill(crs[1][0]);
+				hCrs_m1_p0_T3_top->Fill(crs[7][0]);
+				hCrs_m1_p1_T3_top->Fill(crs[12][0]);
+				hCrs_m2_p2_T3_top->Fill(crs[16][0]);
+			}
+			if(Tr[3][0] == 1){
+				//cout << "tr4" << endl;
+				hCrs_p0_m2_T4_top->Fill(crs[2][0]);
+				hCrs_p0_m1_T4_top->Fill(crs[5][0]);
+				hCrs_p0_p0_T4_top->Fill(crs[8][0]);
+				hCrs_p0_p1_T4_top->Fill(crs[13][0]);
+				hCrs_p1_p2_T4_top->Fill(crs[18][0]);
+				hCrs_p1_p3_T4_top->Fill(crs[21][0]);
+			}
+			if(Tr[4][0] == 1){
+				//cout << "tr5" << endl;
+				hCrs_p0_m2_T5_top->Fill(crs[2][0]);
+				hCrs_p0_m1_T5_top->Fill(crs[5][0]);
+				hCrs_p0_p0_T5_top->Fill(crs[8][0]);
+				hCrs_p0_p1_T5_top->Fill(crs[13][0]);
+				hCrs_m1_p2_T5_top->Fill(crs[17][0]);
+				hCrs_m1_p3_T5_top->Fill(crs[20][0]);
+			}
+			if(Tr[5][0] == 1){
+				//cout << "tr6" << endl;
+				hCrs_p1_m2_T6_top->Fill(crs[3][0]);
+				hCrs_p1_p0_T6_top->Fill(crs[9][0]);
+				hCrs_p1_p1_T6_top->Fill(crs[14][0]);
+				hCrs_p2_p2_T6_top->Fill(crs[19][0]);
+			}
+			if(Tr[6][0] == 1){
+				//cout << "tr7" << endl;
+				hCrs_p2_m2_T7_top->Fill(crs[4][0]);
+				hCrs_p1_p0_T7_top->Fill(crs[9][0]);
+				hCrs_p1_p1_T7_top->Fill(crs[14][0]);
+				hCrs_p2_p2_T7_top->Fill(crs[19][0]);
+			}
+			if(Tr[7][0] == 1){
+				//cout << "tr8" << endl;
+				hCrs_p2_p0_T8_top->Fill(crs[10][0]);
+				hCrs_p2_p1_T8_top->Fill(crs[15][0]);
+			}
+
+
+			//BOTTOM
+			if(Tr[0][1] == 1){
+				hCrs_m2_p0_T1_bot->Fill(crs[6][1]);
+				hCrs_m2_p1_T1_bot->Fill(crs[11][1]);
+			}
+			if(Tr[1][1] == 1){
+				hCrs_m2_m2_T2_bot->Fill(crs[0][1]);
+				hCrs_m1_p0_T2_bot->Fill(crs[7][1]);
+				hCrs_m1_p1_T2_bot->Fill(crs[12][1]);
+				hCrs_m2_p2_T2_bot->Fill(crs[16][1]);
+			}
+			if(Tr[2][1] == 1){
+				hCrs_m1_m2_T3_bot->Fill(crs[1][1]);
+				hCrs_m1_p0_T3_bot->Fill(crs[7][1]);
+				hCrs_m1_p1_T3_bot->Fill(crs[12][1]);
+				hCrs_m2_p2_T3_bot->Fill(crs[16][1]);
+			}
+			if(Tr[3][1] == 1){
+				hCrs_p0_m2_T4_bot->Fill(crs[2][1]);
+				hCrs_p0_m1_T4_bot->Fill(crs[5][1]);
+				hCrs_p0_p0_T4_bot->Fill(crs[8][1]);
+				hCrs_p0_p1_T4_bot->Fill(crs[13][1]);
+				hCrs_p1_p2_T4_bot->Fill(crs[18][1]);
+				hCrs_p1_p3_T4_bot->Fill(crs[21][1]);
+			}
+			if(Tr[4][1 == 1]){
+				hCrs_p0_m2_T5_bot->Fill(crs[2][1]);
+				hCrs_p0_m1_T5_bot->Fill(crs[5][1]);
+				hCrs_p0_p0_T5_bot->Fill(crs[8][1]);
+				hCrs_p0_p1_T5_bot->Fill(crs[13][1]);
+				hCrs_m1_p2_T5_bot->Fill(crs[17][1]);
+				hCrs_m1_p3_T5_bot->Fill(crs[20][1]);
+			}
+			if(Tr[5][1] == 1){
+				hCrs_p1_m2_T6_bot->Fill(crs[3][1]);
+				hCrs_p1_p0_T6_bot->Fill(crs[9][1]);
+				hCrs_p1_p1_T6_bot->Fill(crs[14][1]);
+				hCrs_p2_p2_T6_bot->Fill(crs[19][1]);
+			}
+			if(Tr[6][1] == 1){
+				hCrs_p2_m2_T7_bot->Fill(crs[4][1]);
+				hCrs_p1_p0_T7_bot->Fill(crs[9][1]);
+				hCrs_p1_p1_T7_bot->Fill(crs[14][1]);
+				hCrs_p2_p2_T7_bot->Fill(crs[19][1]);
+			}
+			if(Tr[7][1] == 1){
+				hCrs_p2_p0_T8_bot->Fill(crs[10][1]);
+				hCrs_p2_p1_T8_bot->Fill(crs[15][1]);
+			}
+
+		}
+	}
+//	japp->RootUnLock();
 	return NOERROR;
 }
 
@@ -145,7 +460,112 @@ jerror_t JEventProcessor_BDXMiniBeamMuonCalibration::erun(void) {
 	// changed to give you a chance to clean up before processing
 	// events from the next run number.
 
-	//m_ROOTOutput->AddObject(hBDXMiniCalorimeterEnergyCalibrationBOTTOMene_thr[ii]);
+
+	//TOP histos
+
+	m_ROOTOutput->AddObject(hTrj_all_top);
+	m_ROOTOutput->AddObject(hTrj_all_bot);
+
+	m_ROOTOutput->AddObject(hTrj_corr_top);
+	m_ROOTOutput->AddObject(hTrj_corr_bot);
+
+	m_ROOTOutput->AddObject(hTrj_corr_crs_top);
+	m_ROOTOutput->AddObject(hTrj_corr_crs_bot);
+
+	m_ROOTOutput->AddObject(hCrs_m2_p0_T1_top);
+	m_ROOTOutput->AddObject(hCrs_m2_p1_T1_top);
+
+	m_ROOTOutput->AddObject(hCrs_m2_m2_T2_top);
+	m_ROOTOutput->AddObject(hCrs_m1_p0_T2_top);
+	m_ROOTOutput->AddObject(hCrs_m1_p1_T2_top);
+	m_ROOTOutput->AddObject(hCrs_m2_p2_T2_top);
+
+	m_ROOTOutput->AddObject(hCrs_m1_m2_T3_top);
+	m_ROOTOutput->AddObject(hCrs_m1_p0_T3_top);
+	m_ROOTOutput->AddObject(hCrs_m1_p1_T3_top);
+	m_ROOTOutput->AddObject(hCrs_m2_p2_T3_top);
+
+	m_ROOTOutput->AddObject(hCrs_p0_m2_T4_top);
+	m_ROOTOutput->AddObject(hCrs_p0_m1_T4_top);
+	m_ROOTOutput->AddObject(hCrs_p0_p0_T4_top);
+	m_ROOTOutput->AddObject(hCrs_p0_p1_T4_top);
+	m_ROOTOutput->AddObject(hCrs_p1_p2_T4_top);
+	m_ROOTOutput->AddObject(hCrs_p1_p3_T4_top);
+
+	m_ROOTOutput->AddObject(hCrs_p0_m2_T5_top);
+	m_ROOTOutput->AddObject(hCrs_p0_m1_T5_top);
+	m_ROOTOutput->AddObject(hCrs_p0_p0_T5_top);
+	m_ROOTOutput->AddObject(hCrs_p0_p1_T5_top);
+	m_ROOTOutput->AddObject(hCrs_m1_p2_T5_top);
+	m_ROOTOutput->AddObject(hCrs_m1_p3_T5_top);
+
+	m_ROOTOutput->AddObject(hCrs_p1_m2_T6_top);
+	m_ROOTOutput->AddObject(hCrs_p1_p0_T6_top);
+	m_ROOTOutput->AddObject(hCrs_p1_p1_T6_top);
+	m_ROOTOutput->AddObject(hCrs_p2_p2_T6_top);
+
+	m_ROOTOutput->AddObject(hCrs_p2_m2_T7_top);
+	m_ROOTOutput->AddObject(hCrs_p1_p0_T7_top);
+	m_ROOTOutput->AddObject(hCrs_p1_p1_T7_top);
+	m_ROOTOutput->AddObject(hCrs_p2_p2_T7_top);
+
+	m_ROOTOutput->AddObject(hCrs_p2_p0_T8_top);
+	m_ROOTOutput->AddObject(hCrs_p2_p1_T8_top);
+
+
+
+	//BOTTOM HISTOS
+	m_ROOTOutput->AddObject(hCrs_m2_p0_T1_bot);
+	m_ROOTOutput->AddObject(hCrs_m2_p1_T1_bot);
+
+	m_ROOTOutput->AddObject(hCrs_m2_m2_T2_bot);
+	m_ROOTOutput->AddObject(hCrs_m1_p0_T2_bot);
+	m_ROOTOutput->AddObject(hCrs_m1_p1_T2_bot);
+	m_ROOTOutput->AddObject(hCrs_m2_p2_T2_bot);
+
+	m_ROOTOutput->AddObject(hCrs_m1_m2_T3_bot);
+	m_ROOTOutput->AddObject(hCrs_m1_p0_T3_bot);
+	m_ROOTOutput->AddObject(hCrs_m1_p1_T3_bot);
+	m_ROOTOutput->AddObject(hCrs_m2_p2_T3_bot);
+
+	m_ROOTOutput->AddObject(hCrs_p0_m2_T4_bot);
+	m_ROOTOutput->AddObject(hCrs_p0_m1_T4_bot);
+	m_ROOTOutput->AddObject(hCrs_p0_p0_T4_bot);
+	m_ROOTOutput->AddObject(hCrs_p0_p1_T4_bot);
+	m_ROOTOutput->AddObject(hCrs_p1_p2_T4_bot);
+	m_ROOTOutput->AddObject(hCrs_p1_p3_T4_bot);
+
+	m_ROOTOutput->AddObject(hCrs_p0_m2_T5_bot);
+	m_ROOTOutput->AddObject(hCrs_p0_m1_T5_bot);
+	m_ROOTOutput->AddObject(hCrs_p0_p0_T5_bot);
+	m_ROOTOutput->AddObject(hCrs_p0_p1_T5_bot);
+	m_ROOTOutput->AddObject(hCrs_m1_p2_T5_bot);
+	m_ROOTOutput->AddObject(hCrs_m1_p3_T5_bot);
+
+	m_ROOTOutput->AddObject(hCrs_p1_m2_T6_bot);
+	m_ROOTOutput->AddObject(hCrs_p1_p0_T6_bot);
+	m_ROOTOutput->AddObject(hCrs_p1_p1_T6_bot);
+	m_ROOTOutput->AddObject(hCrs_p2_p2_T6_bot);
+
+	m_ROOTOutput->AddObject(hCrs_p2_m2_T7_bot);
+	m_ROOTOutput->AddObject(hCrs_p1_p0_T7_bot);
+	m_ROOTOutput->AddObject(hCrs_p1_p1_T7_bot);
+	m_ROOTOutput->AddObject(hCrs_p2_p2_T7_bot);
+
+	m_ROOTOutput->AddObject(hCrs_p2_p0_T8_bot);
+	m_ROOTOutput->AddObject(hCrs_p2_p1_T8_bot);
+
+
+
+
+
+
+
+
+
+
+
+
 
 	return NOERROR;
 }
--- src/plugins/calibration/BDXMiniBeamMuonCalibration/JEventProcessor_BDXMiniBeamMuonCalibration.h
+++ src/plugins/calibration/BDXMiniBeamMuonCalibration/JEventProcessor_BDXMiniBeamMuonCalibration.h
@@ -9,6 +9,9 @@
 #define _JEventProcessor_BDXMiniBeamMuonCalibration_
 
 #include <JANA/JEventProcessor.h>
+
+class TH1D;
+class TH2D;
 class JROOTOutput;
 
 class JEventProcessor_BDXMiniBeamMuonCalibration: public jana::JEventProcessor {
@@ -28,8 +31,89 @@ private:
 
 	int m_isFirstCallToBrun;
 
+
+	TH1D *hTrj_all_top;
+	TH1D *hTrj_all_bot;
+	TH2D *hTrj_corr_top;
+	TH2D *hTrj_corr_bot;
+
+	TH2D *hTrj_corr_crs_top;
+	TH2D *hTrj_corr_crs_bot;
+
+	//Crs Histos:
+
+	//T1
+	TH1D *hCrs_m2_p0_T1_top, *hCrs_m2_p1_T1_top;
+	TH1D *hCrs_m2_p0_T1_bot, *hCrs_m2_p1_T1_bot;
+
+	//T2
+	TH1D *hCrs_m2_m2_T2_top, *hCrs_m1_p0_T2_top, *hCrs_m1_p1_T2_top, *hCrs_m2_p2_T2_top;
+	TH1D *hCrs_m2_m2_T2_bot, *hCrs_m1_p0_T2_bot, *hCrs_m1_p1_T2_bot, *hCrs_m2_p2_T2_bot;
+
+	//T3
+	TH1D *hCrs_m1_m2_T3_top, *hCrs_m1_p0_T3_top, *hCrs_m1_p1_T3_top, *hCrs_m2_p2_T3_top;
+	TH1D *hCrs_m1_m2_T3_bot, *hCrs_m1_p0_T3_bot, *hCrs_m1_p1_T3_bot, *hCrs_m2_p2_T3_bot;
+
+	//T4
+	TH1D *hCrs_p0_m2_T4_top, *hCrs_p0_m1_T4_top, *hCrs_p0_p0_T4_top, *hCrs_p0_p1_T4_top, *hCrs_p1_p2_T4_top, *hCrs_p1_p3_T4_top;
+	TH1D *hCrs_p0_m2_T4_bot, *hCrs_p0_m1_T4_bot, *hCrs_p0_p0_T4_bot, *hCrs_p0_p1_T4_bot, *hCrs_p1_p2_T4_bot, *hCrs_p1_p3_T4_bot;
+
+	//T5
+	TH1D *hCrs_p0_m2_T5_top, *hCrs_p0_m1_T5_top, *hCrs_p0_p0_T5_top, *hCrs_p0_p1_T5_top, *hCrs_m1_p2_T5_top, *hCrs_m1_p3_T5_top;
+	TH1D *hCrs_p0_m2_T5_bot, *hCrs_p0_m1_T5_bot, *hCrs_p0_p0_T5_bot, *hCrs_p0_p1_T5_bot, *hCrs_m1_p2_T5_bot, *hCrs_m1_p3_T5_bot;
+
+	//T6
+	TH1D *hCrs_p1_m2_T6_top, *hCrs_p1_p0_T6_top, *hCrs_p1_p1_T6_top, *hCrs_p2_p2_T6_top;
+	TH1D *hCrs_p1_m2_T6_bot, *hCrs_p1_p0_T6_bot, *hCrs_p1_p1_T6_bot, *hCrs_p2_p2_T6_bot;
+
+	//T7
+	TH1D *hCrs_p2_m2_T7_top, *hCrs_p1_p0_T7_top, *hCrs_p1_p1_T7_top, *hCrs_p2_p2_T7_top;
+	TH1D *hCrs_p2_m2_T7_bot, *hCrs_p1_p0_T7_bot, *hCrs_p1_p1_T7_bot, *hCrs_p2_p2_T7_bot;
+
+	//T8
+	TH1D *hCrs_p2_p0_T8_top, *hCrs_p2_p1_T8_top;
+	TH1D *hCrs_p2_p0_T8_bot, *hCrs_p2_p1_T8_bot;
+
 	JROOTOutput *m_ROOTOutput;
 	int m_isMC;
+
+
+	int getCaloIDXFromXY(const int ix, const int iy) {
+
+		if(ix==-2 && iy==-2) return 1;
+		if(ix==-1 && iy==-2) return 2;
+		if(ix==0 && iy==-2) return 3;
+		if(ix==1 && iy==-2) return 4;
+		if(ix==2 && iy==-2) return 5;
+		if(ix==0 && iy==-1) return 6;
+
+		if(ix==-2 && iy==0) return 7;
+		if(ix==-1 && iy==0) return 8;
+		if(ix==0 && iy==0) return 9;
+		if(ix==1 && iy==0) return 10;
+		if(ix==2 && iy==0) return 11;
+
+		if(ix==-2 && iy==1) return 12;
+		if(ix==-1 && iy==1) return 13;
+		if(ix==0 && iy==1) return 14;
+		if(ix==1 && iy==1) return 15;
+		if(ix==2 && iy==1) return 16;
+
+		if(ix==-2 && iy==2) return 17;
+		if(ix==-1 && iy==2) return 18;
+		if(ix==1 && iy==2) return 19;
+		if(ix==2 && iy==2) return 20;
+
+		//if(x==1 && y==3) return 21;
+		//if(x==2 && y==3) return 22;
+
+		if(ix==-1 && iy==3) return 21;
+		if(ix==1 && iy==3) return 22;
+		}
+
+
+
+
 };
 
 #endif // _JEventProcessor_BDXMiniBeamMuonCalibration_
